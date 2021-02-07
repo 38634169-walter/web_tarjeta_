b4424684691eca0a33f69fb5cfb7d6055c2d446e
walter <walterdiaz9418@gmail.com>
2021-02-07 02:46:54 -0300

    agregados en el menu

--- js/menu-responsive.js
+++ js/menu-responsive.js
@@ -6,18 +6,22 @@ let num = 0;
 function mostrar_menu(){
     $('.btn-responsive').click(function(){
         
-        if(num == 0){
-            $('.nav').animate({
-                left:'0'
-            });
-            num = 1;
-        }
-        else{
-            $('.nav').animate({
-                left:'-100%'
-            });
-            num = 0;
-        }
+        $('.nav').animate({
+            left:'0'
+        });
+        $('.btn-responsive').toggleClass('remove-display');
+        $('.xicon-menu').toggleClass('add-display');
 
     });
+
+    $('.xicon-menu').click(function(){
+        $('.nav').animate({
+            left:'-100%'
+        });
+
+        $('.btn-responsive').toggleClass('remove-display');
+        $('.xicon-menu').toggleClass('add-display');
+    });
+
+
 }
